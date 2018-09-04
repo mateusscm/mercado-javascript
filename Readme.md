0cc9365fcf9d64da11afe83cf197a3833a07c515
mateusscm <mateus.miranda@hotmail.com.br>
2018-09-04 18:08:08 -0300

    Primeiro commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+#JavaScript #HTML
+
+Trata-se de um código para treinamento de JavaScript, principalmente a parte de percorrer vetor. Sem utilização de CSS.
+Foi feito o teste de um mercado com apenas 3 itens, o qual o usuário seleciona os itens desejados e suas respectivas quantidades, após isso se deve Gerar Pedido para poder fazer a Geração de Relatório.
+
+O desafio principal seria a não utilização do laço FOR, e sim outras alternativas... uma maneira utilizada de início foi o foreach, e a segunda, foi a utilização de MAP(). Fora essas duas funções, há tbm (mas nao foram utilizada), o for in, for off, reduce e filter.
---
+++ js/script.js
@@ -0,0 +1,78 @@
+var pedidos = [];
+var vendidos = {Bananaa: 0, Macaa: 0, Peraa: 0, Total: 0};
+
+function bananaChange(checkbox){
+	if(checkbox.checked == true){
+		document.getElementById("bananaQtd").style.visibility = "visible";
+	}else{
+		document.getElementById("bananaQtd").style.visibility = "hidden";
+	}
+}
+
+function macaChange(checkbox){
+	if(checkbox.checked == true){
+		document.getElementById("macaQtd").style.visibility = "visible";
+	}else{
+		document.getElementById("macaQtd").style.visibility = "hidden";
+	}
+}
+
+function peraChange(checkbox){
+	if(checkbox.checked == true){
+		document.getElementById("peraQtd").style.visibility = "visible";
+	}else{
+		document.getElementById("peraQtd").style.visibility = "hidden";
+	}
+}
+
+
+
+function logArrayElements(element, index, array){
+	console.log("a[" + index + "] =" + element);
+}
+
+function gerarPedido(){
+	var pedido = {Total: 0};
+	if(document.getElementById("banana").checked){
+		pedido.Bananaa = parseInt(document.getElementById("bananaQtd").value);
+		pedido['Total'] = (1.50 * document.getElementById("bananaQtd").value) + pedido['Total'];
+
+	}
+
+	if(document.getElementById("maca").checked){
+		pedido.Macaa = parseInt(document.getElementById("macaQtd").value);
+		pedido['Total'] = (2.00 * document.getElementById("macaQtd").value) + pedido['Total'];
+	}
+
+	if(document.getElementById("pera").checked){
+		pedido.Peraa = parseInt(document.getElementById("peraQtd").value);
+		pedido['Total'] = (4.00 * document.getElementById("peraQtd").value) + pedido['Total'];
+	}
+
+
+	pedidos.push(pedido);
+	console.log('Pedido');
+	console.log(pedido);
+
+}
+
+function gerarRelatorio(){
+	var vendidos = {Bananaa: 0, Macaa: 0, Peraa: 0, Total: 0};
+	pedidos.map(function(item){
+		if("Bananaa" in item){
+			vendidos['Bananaa'] = vendidos['Bananaa'] + item['Bananaa'];}
+		if("Macaa" in item){
+			vendidos['Macaa'] = vendidos['Macaa'] + item['Macaa'];}	
+		if("Peraa" in item){
+			vendidos['Peraa'] = vendidos['Peraa'] + item['Peraa'];	}	
+
+		vendidos['Total'] = vendidos['Total'] + item['Total'];
+	})
+
+	console.log('Relatorio');
+	console.log(vendidos);
+	document.getElementById('print').innerHTML= 'Bananaa: ' + vendidos['Bananaa'] + '<br>' + 
+												'Macaa: ' + vendidos['Macaa'] + '<br>' + 
+												'Peraa: ' + vendidos['Peraa'] + '<br>' +
+												'Total: R$ ' + vendidos['Total'];
+}
